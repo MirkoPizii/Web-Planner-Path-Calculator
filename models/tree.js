db9ccaba867939b431a045815fb7ba88edd4eb4e
Mirko Pizii <hallino1@gmail.com>
2017-02-09 15:37:54 +0100

    Reformat Code

--- models/tree.js
+++ models/tree.js
@@ -1,21 +1,21 @@
-var mongoose    =   require("mongoose");
+var mongoose = require("mongoose");
 //mongoose.connect('mongodb://localhost:27017/ppc');
 // create instance of Schema
-var Schema =   mongoose.Schema;
+var Schema = mongoose.Schema;
 
 // create Tree schema
-var treeSchema  = new Schema({
-    name: String,
-    splitSize: Number,
-    depthSize: Number,
-    total: Number,
-    vertecesAttributeList: Array,
-    edgesAttributeList: Array,
-    creation: {
-    	type: Date,
-    	default: Date.now
-    },
-    lastOperation: Date
+var treeSchema = new Schema({
+  name: String,
+  splitSize: Number,
+  depthSize: Number,
+  total: Number,
+  vertecesAttributeList: Array,
+  edgesAttributeList: Array,
+  creation: {
+    type: Date,
+    default: Date.now
+  },
+  lastOperation: Date
 });
 
 // create model if not exists.
@@ -25,24 +25,24 @@ var Tree = mongoose.model('Tree', treeSchema);
 module.exports = Tree;
 
 // CREATE TREE
-module.exports.createTree = function(albero, callback){
-	Tree.create(albero, callback)
+module.exports.createTree = function(albero, callback) {
+  Tree.create(albero, callback)
 }
 
 // GET TREES
-module.exports.getTrees = function(callback){
-	Tree.find(callback)
-	//Tree.find(callback).limit(limit)
+module.exports.getTrees = function(callback) {
+  Tree.find(callback)
+  //Tree.find(callback).limit(limit)
 }
 
 // GET TREES
-module.exports.countTrees = function(callback){
-    Tree.count(callback)
+module.exports.countTrees = function(callback) {
+  Tree.count(callback)
 }
 
 // GET TREE BY ID
-module.exports.getTreeById = function(id, callback){
-	Tree.findById(id, callback)
+module.exports.getTreeById = function(id, callback) {
+  Tree.findById(id, callback)
 }
 
 
